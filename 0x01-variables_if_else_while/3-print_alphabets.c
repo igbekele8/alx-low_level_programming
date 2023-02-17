a71a14fa7dc8c96b42bff398715f2d2e59ad79bd
igbkele8 <igbekeleolalekan@gmail.com>
2023-02-17 04:52:27 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints lowercase, uppercase alpabet and a new line
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = 'A' ; i <= 'Z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
