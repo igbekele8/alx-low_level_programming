84294344b7a2fc6149d739da931c257d5af5f240
igbkele8 <igbekeleolalekan@gmail.com>
2023-02-17 05:48:18 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints alphabet lowercase in reverse form and a new line
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
