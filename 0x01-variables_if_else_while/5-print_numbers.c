6766490f7a72031e3d37804277fc4c7ca8fa0b74
igbkele8 <igbekeleolalekan@gmail.com>
2023-02-17 05:31:05 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints all single digit num of base 10 start from 0
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
