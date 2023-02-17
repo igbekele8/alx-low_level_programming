ea595dd750fc55c53bf78b0401c56f05e22a603e
igbkele8 <igbekeleolalekan@gmail.com>
2023-02-17 05:00:34 -0800

    print alphabet except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints lowercase alphabeth and then a new line
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'a' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
